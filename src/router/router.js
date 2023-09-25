75968ce8a37288250e54e038f5cd4ebad9891fc3
FajarAdi25 <fajaarapp@gmail.com>
2023-09-26 04:22:53 +0700

    add slicing login & register

--- src/router/router.js
+++ src/router/router.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "../component/navbar/navbar";
 import Home from "../page/home/home";
 import NotFound from "../page/notFound/notFound";
 import Login from "../page/login/login";
+import Register from "../page/register/register";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route index element={<Home />} />
+          <Route index element={<Navbar />} />
           <Route path="login" element={<Login />} />
-          {/* <Route path="register" element={<Register />} /> */}
+          <Route path="register" element={<Register />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
