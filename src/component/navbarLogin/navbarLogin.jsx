ba03ee81ba85b5ef7c878b5165a88b7d018957d1
FajarAdi25 <fajaarapp@gmail.com>
2023-09-27 15:16:07 +0700

    update some img. Add component carousel, category, productCard. Add home
    Add CSS component & home

---
+++ src/component/navbarLogin/navbarLogin.jsx
@@ -0,0 +1,165 @@
+/* eslint-disable jsx-a11y/role-supports-aria-props */
+import React from "react";
+import "../navbar/navbar.css";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faMagnifyingGlass,
+  faShoppingCart,
+  faSliders,
+  faBell,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="row align-items-center" style={{ width: "100%" }}>
+        <div id="header" className="row align-items-center">
+          <div className="container">
+            <div className="row justify-content-around align-items-center">
+              <Link className="logo col-auto me-4" to={"/"}>
+                <img
+                  src={require("../../asset/img/blanja.png")}
+                  alt="logo-blanja"
+                  height={40}
+                />
+              </Link>
+              <div className="main-nav col ms-4">
+                <div className="row align-items-center d-flex">
+                  <div className="col search">
+                    <input
+                      type="text"
+                      className="form-control decoration-none"
+                      placeholder="Search Product"
+                    />
+                    <FontAwesomeIcon
+                      id="ic-search"
+                      className="ic"
+                      icon={faMagnifyingGlass}
+                      size="lg"
+                    />
+                  </div>
+                  <div className="col-auto filters">
+                    <FontAwesomeIcon
+                      id="ic-filters"
+                      className="ic"
+                      icon={faSliders}
+                      size="lg"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="action col-auto">
+                <div className="row align-items-center d-flex">
+                  <div className="col-auto me-3">
+                    <FontAwesomeIcon
+                      id="ic-shopping-cart"
+                      className="ic"
+                      icon={faShoppingCart}
+                      size="lg"
+                      style={{ cursor: "pointer" }}
+                    />
+                  </div>
+                  <div id="ic-bell" className="col-auto me-3">
+                    <FontAwesomeIcon
+                      className="ic"
+                      icon={faBell}
+                      size="lg"
+                      style={{ cursor: "pointer" }}
+                    />
+                  </div>
+                  <div id="ic-envelope" className="col-auto me-3">
+                    <FontAwesomeIcon
+                      className="ic"
+                      icon={faEnvelope}
+                      size="lg"
+                      style={{ cursor: "pointer" }}
+                    />
+                  </div>
+                  <div id="img-profile" className="col-auto dropdown">
+                    <img
+                      className="img-profile rounded-circle"
+                      src={require("../../asset/img/blanja.png")}
+                      alt="Profile"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    />
+                    <ul className="dropdown-menu dropdown-menu-end justify-content-end text-end">
+                      <li>
+                        <Link to="/profile">
+                          <button className="dropdown-item" type="button">
+                            Profile
+                          </button>
+                        </Link>
+                      </li>
+                      <li>
+                        <hr className="dropdown-divider" />
+                      </li>
+                      <li>
+                        <button
+                          className="dropdown-item"
+                          type="button"
+                          onClick={() => {
+                            localStorage.clear();
+                            window.location.href = "/login";
+                          }}
+                        >
+                          Logout
+                        </button>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="col-auto d-flex justify-content-between align-items-center menu">
+                    <button
+                      className="btn"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseExample"
+                      aria-expanded="false"
+                      aria-controls="collapseExample"
+                    >
+                      <FontAwesomeIcon
+                        icon={faBars}
+                        size="lg"
+                        style={{ color: "white", cursor: "pointer" }}
+                      />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className="row collapse justify-content-end text-end"
+          id="collapseExample"
+        >
+          <div className="card">
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Link
+                  className="text-black text-decoration-none mb-3 text-center"
+                  to="/login"
+                >
+                  Login
+                </Link>
+              </li>
+              <li className="list-group-item">
+                <Link
+                  className="text-black text-decoration-none mb-3 text-center"
+                  to="/register"
+                >
+                  Register
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
--- src/page/login/login.jsx
+++ src/page/login/login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "../../asset/globalStyle.css";
 import { Link, useNavigate } from "react-router-dom";
 import { Tab, Tabs } from "react-bootstrap";
-import style from "./styleLogin.css"
+import style from "./styleLogin.css";
 import axios from "axios";
 import { url } from "../../redux/baseUrl/url";
 import Swal from "sweetalert2";
@@ -11,64 +11,58 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errmsg, setErrmsg] = useState("");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleLoginCustomer = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`${url}/customer/login`,{
+      const res = await axios.post(`${url}/customer/login`, {
         email: email,
         password: password,
       });
-      localStorage.setItem("token",res.data.token);
-      localStorage.setItem("userId",res.data.data.customer_id);
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("userId", res.data.data.customer_id);
       Swal.fire({
         text: "Gratz Login succes brother",
         icon: "success",
       });
-      navigate("/")
-      
-       
-      console.log(res.data.data)
+      navigate("/");
+      console.log(res.data.data);
     } catch (err) {
-      if (err){
-        setErrmsg(err.response.data.message)
+      if (err) {
+        setErrmsg(err.response.data.message);
         Swal.fire({
           text: errmsg,
           icon: "error",
         });
       }
     }
-  }
-
+  };
   const handleLoginSeller = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`${url}/seller/login`,{
+      const res = await axios.post(`${url}/seller/login`, {
         email: email,
         password: password,
       });
-      localStorage.setItem("token",res.data.token);
-      localStorage.setItem("userId",res.data.data.seller_id);
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("userId", res.data.data.seller_id);
       Swal.fire({
         text: "Gratz Login succes brother",
         icon: "success",
       });
-      navigate("/")
-      
-       
-      console.log(res.data.data)
+      navigate("/");
+      console.log(res.data.data);
     } catch (err) {
-      if (err){
-        setErrmsg(err.response.data.message)
+      if (err) {
+        setErrmsg(err.response.data.message);
         Swal.fire({
           text: errmsg,
           icon: "error",
         });
       }
     }
-  }
-
+  };
 
   return (
     <>
@@ -85,132 +79,142 @@ function Login() {
               <b>Please login with your account</b>
             </p>
             <Tabs
-            justify variant="pills"
-            defaultActiveKey="Customer"
-            transition={false}
-            id="noanim-tab-example"
-            className="btn-group position-relative top-0 start-50 translate-middle mx-auto mt-3 d-flex justify-content-center align-items-center"
+              justify
+              variant="pills"
+              defaultActiveKey="Customer"
+              transition={false}
+              id="noanim-tab-example"
+              className="btn-group position-relative top-0 start-50 translate-middle mx-auto mt-3 d-flex justify-content-center align-items-center"
               role="group"
               aria-label="Basic radio toggle button group"
-          >
-            <Tab eventKey="Customer" title="Customer" style={style} >
-              <div className="container ">
-                <div className="row row-cols-1 row-cols-md-3 g-4 d-flex justify-content-center mt-3 ">
-               
-                <div className="d-flex justify-content-center mt-3">
-                <form
             >
-              <div style={{width:"43vh",display:"flex",flexDirection:"column"}}>
-                <label
-                  htmlFor="exampleInputEmail1"
-                  className="form-label"
-                ></label>
-                <input
-                  type="email"
-                  className="form-control form-control-lg"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                  placeholder="Email"
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              <div className="mb-3">
-                <label
-                  htmlFor="exampleInputPassword1"
-                  className="form-label"
-                ></label>
-                <input
-                  type="password"
-                  className="form-control  form-control-lg"
-                  id="exampleInputPassword1"
-                  placeholder="Password"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <small>
-                <Link
-                  to={"/forgotPassword"}
-                  className="d-block text-decoration-none text-end text-danger mt-3"
-                >
-                  Forgot Password?
-                </Link>
-              </small>
+              <Tab eventKey="Customer" title="Customer" style={style}>
+                <div className="container ">
+                  <div className="row row-cols-1 row-cols-md-3 g-4 d-flex justify-content-center mt-3 ">
+                    <div className="d-flex justify-content-center mt-3">
+                      <form>
+                        <div
+                          style={{
+                            width: "43vh",
+                            display: "flex",
+                            flexDirection: "column",
+                          }}
+                        >
+                          <label
+                            htmlFor="exampleInputEmail1"
+                            className="form-label"
+                          ></label>
+                          <input
+                            type="email"
+                            className="form-control form-control-lg"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                            placeholder="Email"
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
+                        </div>
+                        <div className="mb-3">
+                          <label
+                            htmlFor="exampleInputPassword1"
+                            className="form-label"
+                          ></label>
+                          <input
+                            type="password"
+                            className="form-control  form-control-lg"
+                            id="exampleInputPassword1"
+                            placeholder="Password"
+                            onChange={(e) => setPassword(e.target.value)}
+                          />
+                        </div>
+                        <small>
+                          <Link
+                            to={"/forgotPassword"}
+                            className="d-block text-decoration-none text-end text-danger mt-3"
+                          >
+                            Forgot Password?
+                          </Link>
+                        </small>
 
-              <div className="d-grid mt-4">
-                <button
-                  type="submit"
-                  className="btn btn-danger btn-lg rounded-pill"
-                  onClick={handleLoginCustomer}
-                >
-                  Login
-                </button>
-              </div>
-            </form>
+                        <div className="d-grid mt-4">
+                          <button
+                            type="submit"
+                            className="btn btn-danger btn-lg rounded-pill"
+                            onClick={handleLoginCustomer}
+                          >
+                            Login
+                          </button>
+                        </div>
+                      </form>
+                    </div>
                   </div>
-              </div>
-              </div>
-            </Tab>
-            <Tab eventKey="Seller" title="Seller" style={{ height: "auto" }}>
-              <div className="container ">
-                <div className="row row-cols-1 row-cols-md-3 g-4 d-flex justify-content-center mt-3 ">
-               
-                <div className="d-flex justify-content-center mt-3">
-                <form
-              onSubmit={(event) => {
-                event.preventDefault();
-              }}
-            >
-              <div style={{width:"43vh",display:"flex",flexDirection:"column"}}>
-                <label
-                  htmlFor="exampleInputEmail1"
-                  className="form-label"
-                ></label>
-                <input
-                  type="email"
-                  className="form-control form-control-lg"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                  placeholder="Email"
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              <div className="mb-3">
-                <label
-                  htmlFor="exampleInputPassword1"
-                  className="form-label"
-                ></label>
-                <input
-                  type="password"
-                  className="form-control  form-control-lg"
-                  id="exampleInputPassword1"
-                  placeholder="Password"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <small>
-                <Link
-                  to={"/forgotPassword"}
-                  className="d-block text-decoration-none text-end text-danger mt-3"
-                >
-                  Forgot Password?
-                </Link>
-              </small>
+                </div>
+              </Tab>
+              <Tab eventKey="Seller" title="Seller" style={{ height: "auto" }}>
+                <div className="container ">
+                  <div className="row row-cols-1 row-cols-md-3 g-4 d-flex justify-content-center mt-3 ">
+                    <div className="d-flex justify-content-center mt-3">
+                      <form
+                        onSubmit={(event) => {
+                          event.preventDefault();
+                        }}
+                      >
+                        <div
+                          style={{
+                            width: "43vh",
+                            display: "flex",
+                            flexDirection: "column",
+                          }}
+                        >
+                          <label
+                            htmlFor="exampleInputEmail1"
+                            className="form-label"
+                          ></label>
+                          <input
+                            type="email"
+                            className="form-control form-control-lg"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                            placeholder="Email"
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
+                        </div>
+                        <div className="mb-3">
+                          <label
+                            htmlFor="exampleInputPassword1"
+                            className="form-label"
+                          ></label>
+                          <input
+                            type="password"
+                            className="form-control  form-control-lg"
+                            id="exampleInputPassword1"
+                            placeholder="Password"
+                            onChange={(e) => setPassword(e.target.value)}
+                          />
+                        </div>
+                        <small>
+                          <Link
+                            to={"/forgotPassword"}
+                            className="d-block text-decoration-none text-end text-danger mt-3"
+                          >
+                            Forgot Password?
+                          </Link>
+                        </small>
 
-              <div className="d-grid mt-4">
-                <button
-                  type="submit"
-                  className="btn btn-danger btn-lg rounded-pill"
-                  onClick={handleLoginSeller}
-                >
-                  Login
-                </button>
-              </div>
-            </form>
+                        <div className="d-grid mt-4">
+                          <button
+                            type="submit"
+                            className="btn btn-danger btn-lg rounded-pill"
+                            onClick={handleLoginSeller}
+                          >
+                            Login
+                          </button>
+                        </div>
+                      </form>
+                    </div>
                   </div>
-              </div>
-              </div>
-            </Tab>
-          </Tabs>
+                </div>
+              </Tab>
+            </Tabs>
             {/* <div
               className="btn-group position-relative top-0 start-50 translate-middle mx-auto mt-3"
               role="group"
